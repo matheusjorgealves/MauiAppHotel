270396ff23a3181ec13a2517b71730d9cbc972f2
Matheus Jorge <matheusjorgealves09@gmail.com>
2026-05-11 12:26:16 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState) // cria a janela principal do aplicativo
+        {
+            var window = new Window(new AppShell()); // cria uma nova janela e define como principal janela do aplicativo
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window; // retorna a janela criada
+        }
+    }
+}
